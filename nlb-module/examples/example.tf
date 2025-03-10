11fb06fe97b00201f3ccb0852a0210be3ed2d088
chan-764 <chandanuikey19@gmail.com>
2025-03-10 10:12:58 +0530

    Create example.tf

---
+++ nlb-module/examples/example.tf
@@ -0,0 +1,22 @@
+module "nlb" {
+  source              = "../"  # Instead of "../nlb-module"
+  nlb_name           = "my-nlb"
+  subnet_ids         = ["subnet-0f4f46d382289a9ce", "subnet-0bedbd0993384f7c8"]
+  security_group_ids = ["sg-04feb8d9b6954abb8"]
+  vpc_id             = "vpc-0fca7dde42f4e16bb"
+  target_group_name  = "my-target-group"
+  ec2_instance_ids   = ["i-0faeef9e1c39c29fd", "i-0c34c4b7ca080c7ef"]
+#  certificate_arn    = "arn:aws:acm:us-west-2:985539773806:certificate/cb2b5eaf-561c-4e37-8c81-6301025c3e15"
+}
+
+module "nlb_2" {
+  source              = "../"  # Calling the same module to create another NLB
+  nlb_name           = "nlb-two"
+  subnet_ids         = ["subnet-0f4f46d382289a9ce", "subnet-0bedbd0993384f7c8"]
+  security_group_ids = ["sg-04feb8d9b6954abb8"]
+  vpc_id             = "vpc-0fca7dde42f4e16bb"
+  target_group_name  = "target-group-2"
+  ec2_instance_ids   = ["i-0faeef9e1c39c29fd", "i-0c34c4b7ca080c7ef"]
+ # certificate_arn    = "arn:aws:acm:us-west-2:985539773806:certificate/cb2b5eaf-561c-4e37-8c81-6301025c3e15"
+
+}
