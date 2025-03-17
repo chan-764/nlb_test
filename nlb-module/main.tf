12248e04861912a70fd1c8e1a56613c252a35570
chan-764 <chandanuikey19@gmail.com>
2025-03-17 11:06:44 +0530

    Update main.tf

--- nlb-module/main.tf
+++ nlb-module/main.tf
@@ -20,13 +20,13 @@ resource "aws_lb_target_group_attachment" "tg_attachment" {
   target_id        = var.ec2_instance_ids[count.index]
 }
 
-# resource "aws_lb_listener" "nlb_listener" {
-#  load_balancer_arn = aws_lb.nlb.arn
-#  port              = 80
-#  protocol          = "TCP"
-#
-#  default_action {
-#    type             = "forward"
-#    target_group_arn = aws_lb_target_group.tg.arn
-#  }
-#}
+ resource "aws_lb_listener" "nlb_listener" {
+  load_balancer_arn = aws_lb.nlb.arn
+  port              = 80
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
